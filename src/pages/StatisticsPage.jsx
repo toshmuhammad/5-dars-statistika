8c3f2bd1c22034dca81f03d50ea213bc420810a2
toshmuhammad <toshmuhammadrafiqov@gmail.com>
2025-05-30 13:25:56 +0500

    .

--- src/pages/StatisticsPage.jsx
+++ src/pages/StatisticsPage.jsx
@@ -1,15 +1,19 @@
 import { useStatistics } from "../hooks/useStatistics";
 
 export default function StatisticsPage() {
-  const { data, isLoading, isError, error } = useStatistics();
+    const { data, isLoading, isError, error } = useStatistics();
 
-  if (isLoading) return <div>1-daqiqa.</div>;
-  if (isError) return <div>Xatolik: {error.message}</div>;
+    if (isLoading) return <div className="flex justify-center items-center h-32">
+        <span className="text-4xl font-bold animate-pulse">
+            1-daqiqa.
+        </span>
+    </div>;
+    if (isError) return <div>Xatolik: {error.message}</div>;
 
-  return (
-    <div>
-      <h2>Statistika</h2>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Statistika</h2>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    );
 }
